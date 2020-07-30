0e661887dfb9ac1a5a715791c07c62d712742485
puppymoon <40927060+puppymoon@users.noreply.github.com>
2020-07-30 23:01:51 +0800

    first commit

---
+++ src/main/java/com/springboot/https/requestSSL/RequestSslApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.https.requestSSL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestSslApplication.class, args);
+	}
+
+}
